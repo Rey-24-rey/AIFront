1c95ac576d8d7936cec761adc40d9aaef6e7286f
Rey-24-rey <h.kashazhum@gmail.com>
2025-02-19 18:29:53 +0300

    ui:improvements

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,22 @@ function App() {
       <div className="min-h-screen flex flex-col items-center justify-center bg-black bg-opacity-100 backdrop-blur-lg text-green-400">
         <div className="max-w-5xl p-11 text-center border border-green-500 rounded-2xl shadow-lg bg-gray-900 bg-opacity-30">
           <h1 className="text-4xl font-extrabold text-green-400 mb-6 drop-shadow-lg">
-            Welcome to Financial Data Analytics
+            Financial Data Analytics
           </h1>
           <p className="text-lg text-green-300 mb-6">
-            Experience the power of AI-driven financial insights. Upload your financial report and get deep analytics instantly.
+            AI-driven financial insights. Get deep analytics instantly.
           </p>
 
           <div className="border border-green-900 p-8 rounded-lg bg-black bg-opacity-200 shadow-inner">
-            <h2 className="text-3xl font-semibold text-green-500 mb-4">Required Data & Expected Output</h2>
+            <h2 className="text-3xl font-semibold text-green-500 mb-4">Required Data</h2>
             <p className="text-green-300">
-              <strong>Excel Format Requirements:</strong> Your file should contain:
+              <strong>Excel Format Requirements</strong>
             </p>
             <ul className="list-disc list-inside text-yellow-300 mt-4">
-              <li><strong>Product:</strong> Product Name</li>
-              <li><strong>Date:</strong> Sale date (YYYY-MM-DD) Daily, Weekly, Monthly, Yearly</li>
-              <li><strong>Sales:</strong> Sales value</li>
-              <li><strong>cogs:</strong> Cost of goods sold</li>
+              <p><strong>Product:</strong> Product Name</p>
+              <p><strong>Date:</strong> Sale date (YYYY-MM-DD) Either Daily, Weekly, Monthly, Yearly</p>
+              <p><strong>Sales:</strong> Sales value</p>
+              <p><strong>cogs:</strong> Cost of goods sold</p>
             </ul>
           </div>
 
